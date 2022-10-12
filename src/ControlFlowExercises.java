fc6e5a4d5fc90b0b6a5c37fdc1bfd13fbdfb8811
Fernando Lopez <flopez00032@gmail.com>
2022-10-12 16:43:07 -0500

    finished loop exercise 1

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,12 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        String userString = "";
+        while(i <= 15){
+            userString += i+" ";
+            i++;
+        }
+        System.out.println(userString);
+
+    }
+}
