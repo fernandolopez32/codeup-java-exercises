238b2aa9692548f5d785e8909b5f28c5a62447b9
Fernando Lopez <flopez00032@gmail.com>
2022-10-12 16:59:11 -0500

    finished loop exercise 2 not done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,20 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        String userString = "";
-        while(i <= 15){
-            userString += i+" ";
+//        int i = 5;
+//        String userString = "";
+//        while(i <= 15){
+//            userString += i+" ";
+//            i++;
+//        }
+//        System.out.println(userString);
+        int i = 0;
+        do{
+            if(i%2 ==0){
+            System.out.println(i*i);
+            }
             i++;
-        }
-        System.out.println(userString);
+
+        } while(i <= 100);
 
     }
 }
