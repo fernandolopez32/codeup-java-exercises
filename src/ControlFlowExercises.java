2a8663729f4f2ee26cc2162a0895ab7cb000f7bd
Fernando Lopez <flopez00032@gmail.com>
2022-10-13 09:33:19 -0500

    finished loop exercise fizz buzz for real

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -25,13 +25,15 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i <= 100; i++){
             if(i % 3 == 0 && i % 5 == 0) {
                     System.out.println("FizzBuzz");
                 } else if (i % 3 == 0) {
                 System.out.println("Fizz");
             } else if(i % 5 == 0){
                 System.out.println("Buzz");
+            } else {
+                System.out.println(i);
             }
         }
 
