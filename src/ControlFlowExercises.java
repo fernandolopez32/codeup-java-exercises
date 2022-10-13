f9973b7b0adc8706ccfc3772c3f669725eba5f18
Fernando Lopez <flopez00032@gmail.com>
2022-10-13 14:47:26 -0500

    grades walkthrough

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -46,28 +46,48 @@ public class ControlFlowExercises {
 
 
 
-        System.out.println("What number would you like to go to?");
-        int userNumber = sc.nextInt();
-        sc.nextLine();
-        String userContinue = "yes";
+//        System.out.println("What number would you like to go to?");
+//        int userNumber = sc.nextInt();
+//        sc.nextLine();
+//        String userContinue = "yes";
+//
+//        while (userContinue.equals("yes")||userContinue.equals("y")) {
+//            System.out.println("Here is your table!\n\n number | squared | cubed \n\n ------ | ------- | -----");
+//            for (int i = 1; i <= userNumber; i++) {
+//                int square = i * i;
+//                int cubed = i * i * i;
+////                System.out.println(i + "       | " + square + "       |  " + cubed);
+//                System.out.printf("%-7d | %-7d | %-5d %n",i,square,cubed);
+//                }
+//            System.out.println("Would you like to continue? Yes/No");
+//            userContinue = sc.nextLine().toLowerCase();
+//            if(userContinue.equals("no")||userContinue.equals("n")){break;}
+//            System.out.println("To what number would you like to count now?");
+//            userNumber = sc.nextInt();
+//            sc.nextLine();
+//            }
 
-        while (userContinue.equals("yes")||userContinue.equals("y")) {
-            System.out.println("Here is your table!\n\n number | squared | cubed \n\n ------ | ------- | -----");
-            for (int i = 1; i <= userNumber; i++) {
-                int square = i * i;
-                int cubed = i * i * i;
-//                System.out.println(i + "       | " + square + "       |  " + cubed);
-                System.out.printf("%-7d | %-7d | %-5d %n",i,square,cubed);
-                }
-            System.out.println("Would you like to continue? Yes/No");
-            userContinue = sc.nextLine().toLowerCase();
-            if(userContinue.equals("no")||userContinue.equals("n")){break;}
-            System.out.println("To what number would you like to count now?");
-            userNumber = sc.nextInt();
-            sc.nextLine();
+
+        String userChoice = "y";
+        while(userChoice.equals("y")) {
+            System.out.println("Enter a numerical grade from 0 to 100: ");
+            byte grade = sc.nextByte();
+            char letterGrade;
+            if (grade >= 0 && grade <= 59) {
+                letterGrade = 'F';
+            } else if (grade > 59 && grade <= 66) {
+                letterGrade = 'D';
+            } else if (grade > 66 && grade <= 79) {
+                letterGrade = 'C';
+            } else if (grade > 79 && grade <= 87) {
+                letterGrade = 'B';
+            } else {
+                letterGrade = 'A';
             }
-
-
+            System.out.println("The letter grade is " + letterGrade);
+            System.out.println("would youlike to enter another grade?");
+            userChoice = sc.next();
+        }
 
 
         }
