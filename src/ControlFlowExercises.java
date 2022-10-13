4266dbe4306ca5f446da85d5010b6ba82b623052
Fernando Lopez <flopez00032@gmail.com>
2022-10-13 09:12:35 -0500

    finished loop exercise 2 refined answer

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -7,18 +7,18 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 //        System.out.println(userString);
+
+
         long i =2;
         System.out.println(i);
         do{
-
-            if(i%2== 0) {
-//                System.out.println(i);
-            }
             i*=i;
             System.out.println(i);
+        } while(i < 65536);
 
 
-        } while(i <= 1000000);
+
+
 
     }
 }
