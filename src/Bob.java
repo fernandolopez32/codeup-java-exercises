0041a4b8cfbf4d6d5e8e96e84302a3885e258a49
Fernando Lopez <flopez00032@gmail.com>
2022-10-14 11:34:24 -0500

    Bob exercise done

--- src/Bob.java
+++ src/Bob.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Talk to Bob here:");
-        String userInput = sc.nextLine();
-//        while (!userInput.equals("im done")) {
+        String userInput;
+        System.out.println("Whats up im bob...");
+        do {
+            userInput = sc.nextLine();
             if (userInput.endsWith("?")) {
                 System.out.println("Sure.");
             } else if (userInput.endsWith("!")) {
                 System.out.println("Whoa, chill out!");
             } else if (userInput.equals("")) {
                 System.out.println("Fine be that way");
+            }else if (userInput.equals("Will computers take over the world?")) {
+                System.out.println("Its inevitable");
             } else {
                 System.out.println("Whatever");
             }
-//        }
+
+        }while (!userInput.equals("okay bye"));
+        System.out.println("bye dude");
 
 //        switch (userInput) {
 //            case userInput.endsWith -> System.out.println("One vanilla coming right up");
