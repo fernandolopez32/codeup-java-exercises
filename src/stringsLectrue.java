bae57ab551632dcf8e7a6e0c9d89f67671f7b4a9
Fernando Lopez <flopez00032@gmail.com>
2022-10-14 09:29:26 -0500

    string lecture next day

--- src/stringsLectrue.java
+++ src/stringsLectrue.java
@@ -2,23 +2,40 @@ import java.util.Scanner;
 
 public class stringsLectrue {
     public static void main(String[] args) {
-        String teacher = "teacher";
-        String javier = "teacher";
-//        String doc = javier+teacher;
-        System.out.println(teacher.hashCode());
-        System.out.println(javier.hashCode());
-//        System.out.println(doc);
-//        System.out.println(doc.hashCode());
-        teacher = "not teacher";
-        System.out.println(teacher);
-        System.out.println(teacher.hashCode());
 
-        System.out.println(teacher == javier);
 
         Scanner sc = new Scanner(System.in);
-        String userInput = sc.next();
 
-        System.out.println(javier.equals(userInput));
+//        String javier = "Javier";
+//        String alsoJavier = "Javier";
+//        String doc = javier+teacher;
+//        System.out.println(teacher.hashCode());
+//        System.out.println(javier.hashCode());
+////        System.out.println(doc);
+////        System.out.println(doc.hashCode());
+//        teacher = "not teacher";
+//        System.out.println(teacher);
+//        System.out.println(teacher.hashCode());
+//
+//        System.out.println(teacher == javier);
 
-    }
-}
+        String javier = "Javier";
+        System.out.println(Integer.toHexString((System.identityHashCode(javier))));
+        String alsoJavier = "Javier";
+
+        System.out.println(Integer.toHexString((System.identityHashCode(alsoJavier))));
+
+        javier = javier +" Ruedas";
+        alsoJavier = javier;
+
+        System.out.println(alsoJavier);
+        System.out.println(javier);
+        System.out.println(Integer.toHexString(System.identityHashCode(alsoJavier)));
+
+        String letsTryThis = "Javier";
+        System.out.println(Integer.toHexString(System.identityHashCode(letsTryThis)));
+
+
+
+    }// end of main
+}// end of stringLecture
