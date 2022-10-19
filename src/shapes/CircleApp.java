7d2098a34f9969080d2474a0646e03346fd40179
Fernando Lopez <flopez00032@gmail.com>
2022-10-18 20:45:02 -0500

    trying to do bonus

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -8,16 +8,13 @@ public class CircleApp {
     public static void main(String[] args) {
         Input input = new Input();
         Circle circle = new Circle();
+        Scanner sc = new Scanner(System.in);
+            String userResponse;
 
-            System.out.println("Enter the radius of your circle");
-            circle.setRadius(input.getDouble());
+                System.out.println("Enter the radius of your circle");
+                circle.setRadius(input.getDouble());
 
-            System.out.println("Would you like to make another circle?");
-
-
-
-
-
+//                System.out.println("Would you like to make another circle?");
 
 
     }
