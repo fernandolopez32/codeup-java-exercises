42eb509b1fb638a3fb7b0f513440d449cf63c75e
Fernando Lopez <flopez00032@gmail.com>
2022-10-20 11:38:56 -0500

    making car test

---
+++ src/vehicles/CarTest.java
@@ -0,0 +1,9 @@
+package vehicles;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+        Car honda = new Car();
+        honda.makeNoise();
+    }
+}
---
+++ src/vehicles/Vehicle.java
@@ -0,0 +1,27 @@
+package vehicles;
+
+public class Vehicle {
+    //Create a vehicles.Vehicle class with two properties: a name instance variable and a makeNoise() method.
+    //
+    //
+
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void makeNoise(){
+        System.out.println("Room room");
+    }
+
+
+
+
+
+}
