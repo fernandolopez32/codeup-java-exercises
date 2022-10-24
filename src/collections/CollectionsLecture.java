e6b0e4a4b04af84bdfda18fedcfa08cabb2f7d8b
Fernando Lopez <flopez00032@gmail.com>
2022-10-24 10:03:26 -0500

    monster class imported

---
+++ src/collections/CollectionsLecture.java
@@ -0,0 +1,18 @@
+package collections;
+import rpg.Monster;
+
+
+// DATA STRUCTURES
+
+// list Data Structures are order oriented
+
+
+import java.util.ArrayList;
+
+// ARRAY LIST is a List that is in an array style
+public class CollectionsLecture {
+
+    public static void main(String[] args) {
+        ArrayList<Monster> monsterArrayList = new ArrayList<>();
+    }
+}
