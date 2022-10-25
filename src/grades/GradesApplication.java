29029fa58e44442d6158787fce0f2d1e64c5617d
Fernando Lopez <flopez00032@gmail.com>
2022-10-25 10:33:12 -0500

    exercise finished moving on to the bonusi

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -11,20 +11,20 @@ public class GradesApplication {
         Input input = new Input();
 
 
-        HashMap<String,Students> students = new HashMap<>();
+        HashMap<String, Students> students = new HashMap<>();
 
         Students aaron = new Students("Aaron");
-        aaron.addGrade(68,89,90);
+        aaron.addGrade(68, 89, 90);
 
         Students stephen = new Students("Stephen");
-        stephen.addGrade(99,100,100);
+        stephen.addGrade(99, 100, 100);
 
         Students fernando = new Students("Fernando");
-        fernando.addGrade(87,48,89);
+        fernando.addGrade(87, 48, 89);
 
-        students.put("Fernandolopez32",fernando);
-        students.put("Ayronero",aaron);
-        students.put("SuperMegaMan",stephen);
+        students.put("Fernandolopez32", fernando);
+        students.put("Ayronero", aaron);
+        students.put("SuperMegaMan", stephen);
 
 
         // making hashmap into a list
@@ -32,27 +32,32 @@ public class GradesApplication {
 
         Collections.sort(gitHubUserNames);
 
-        System.out.printf("Welcome!%n%nHere are the GitHub usernames of our students: %n%n%s%n",gitHubUserNames);
+        System.out.printf("Welcome!%n%nHere are the GitHub usernames of our students:%n%n%s%n", gitHubUserNames);
+        String userSelect;
 
-        String userSelect = input.getString();
 
-        System.out.println("What student would you like to see more information on?");
-        switch (userSelect.toLowerCase()){
-            case "ayronero" -> System.out.printf("%s %n %.2f %n",aaron.getName(),aaron.getGradeAverage());
 
-            case "fernandolopez32" -> System.out.printf("%s %n %.2f %n",fernando.getName(),fernando.getGradeAverage());
-            case "supermegaman" -> System.out.printf("%s %n %.2f %n",stephen.getName(),stephen.getGradeAverage());
-            default -> System.out.printf("Sorry, no student found with the GitHub username of \"%s\".",userSelect);
+        String userResp;
+
+
+        do {
+            userSelect = input.getString("What student would you like to see more information on?");
+            switch (userSelect) {
+                case "Ayronero" -> System.out.printf("Name:%s - GitHub Username: %s %nCurrent Average:%.2f %n%n", aaron.getName(),userSelect, aaron.getGradeAverage());
+
+                case "Fernandolopez32" -> System.out.printf("Name:%s - GitHub Username: %s %nCurrent Average:%.2f %n%n", fernando.getName(),userSelect, fernando.getGradeAverage());
+
+                case "SuperMegaMan" -> System.out.printf("Name:%s - GitHub Username: %s %nCurrent Average:%.2f %n%n", stephen.getName(),userSelect, stephen.getGradeAverage());
+
+                default -> System.out.printf("Sorry, no student found with the GitHub username of \"%s\".%n%n", userSelect);
+            }
+
+            userResp = input.getString("Would you like to see another student?");
+        } while (userResp.equals("y"));
+
+        if (!userResp.equals("y")){
+            System.out.println("Goodbye, and have a wonderful day!");
         }
-
-
-
-//        for(String username: gitHubUserNames ){
-//            Students students1 = gitHubUserNames.get(students);
-//            System.out.println(students.get(gitHubUserNames).getName());
-//        }
-
-
 
 
 
