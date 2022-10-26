e78921db8c8e287a615d4d75d0c59ec3a4ab173c
Fernando Lopez <flopez00032@gmail.com>
2022-10-26 14:07:52 -0500

    try catch thing exercise

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -6,12 +6,20 @@ public class InputTest {
 //        Input.getString();
 //        Input.yesNo();
 //        Input.getInt(1,10);
-        Input input = new Input();
+//        Input input = new Input();
 //        System.out.println(input.getString());
 //        System.out.println(input.yesNo());
 //        System.out.println(input.getInt(2,6));
 //        System.out.println(input.getInt());
 //        System.out.println(input.getDouble());
-        System.out.println(input.getDouble()+" Scan your next item");
+//        System.out.println(input.getDouble()+" Scan your next item");
+        justinInput jus = new justinInput();
+
+        int myNumber = jus.getInt();
+        System.out.println(myNumber);
+        double myDouble = jus.getDouble();
+        System.out.println(myDouble);
+
+
     }// end of main
 }// end of InputTest class
