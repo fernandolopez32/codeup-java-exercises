787e53c99b6d8fc90afee27f713855740ddca0a0
Fernando Lopez <flopez00032@gmail.com>
2022-11-01 14:41:34 -0500

    doc rob lecture

---
+++ src/DocRob/Fibinacci.java
@@ -0,0 +1,19 @@
+package DocRob;
+
+public class Fibinacci {
+    public static long  fib(long n){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1) {
+            return 1;
+        }
+            return fib(n-1) + fib(n-2);
+
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        System.out.println(fib(10));
+    }
+}
--- src/assessmentPrep/PetDogTest.java
+++ src/assessmentPrep/PetDogTest.java
@@ -1,8 +1,18 @@
 package assessmentPrep;
 
 public class PetDogTest {
+//    public static void main(String[] args) {
+////        PetDog tobi = new PetDog("Tobi","dog",true);
+////        System.out.println(tobi.snuggle());
+//
+//
+//    }
+
     public static void main(String[] args) {
-        PetDog tobi = new PetDog("Tobi","dog",true);
-        System.out.println(tobi.snuggle());
+        int[][] primes = new int[3][3];
+        primes[2][2] = 1;
+
+        System.out.println(primes[1][2]);
     }
+
 }
