f5855c729a9c2d4a18d21d5e066edc393c6fec8b
Fernando Lopez <flopez00032@gmail.com>
2022-11-01 15:42:02 -0500

    doc rob lecture recurese

--- src/DocRob/Fibinacci.java
+++ src/DocRob/Fibinacci.java
@@ -11,6 +11,27 @@ public class Fibinacci {
             return fib(n-1) + fib(n-2);
 
     }
+//    private static long[] memoTable = new long[999999999];
+//    public static long  fib(long n){
+//
+//        if(n == 0){
+//            return 0;
+//        }
+//        if(n == 1) {
+//            return 1;
+//        }
+//        if(memoTable[n] ! = 0){
+//
+//        }
+//            return fib(n-1) + fib(n-2);
+//
+//    }
+
+    // escape is number is smaller than any other number
+    public static int min(int[] n){
+        return  0;
+    }
+
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
